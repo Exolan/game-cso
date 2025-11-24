1e110d3a557eb0a04a57ec4bad57c535dd02d47f
Exolan <evjekddd@mail.ru>
2025-11-24 19:40:10 +0300

    fix: create src server

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = "http://192.168.1.14:3001";
+const SOCKET_URL = "http://172.18.208.1:3001";
 
 export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false,
